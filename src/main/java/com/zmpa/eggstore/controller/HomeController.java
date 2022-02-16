c6a48491841659a8014e6b152d26dc383114f413
Zaira Popoca <zairapl12@gmail.com>
2022-02-16 04:59:24 -0600

    mostrar vista del producto al usuario

--- src/main/java/com/zmpa/eggstore/controller/HomeController.java
+++ src/main/java/com/zmpa/eggstore/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.zmpa.eggstore.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.zmpa.eggstore.service.ProductoService;
@@ -11,6 +14,9 @@ import com.zmpa.eggstore.service.ProductoService;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger log = LoggerFactory.getLogger(HomeController.class);			//imprime los resultados en la terminal
+	
 	
 	@Autowired
 	private ProductoService productoService;				//permite obtener todos los productos
@@ -21,6 +27,12 @@ public class HomeController {
 		model.addAttribute("productos", productoService.findAll());
 		
 		return "usuario/index";
+	}
+	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id) {
+		log.info("id enviado como parametro {}", id);
+		return "usuario/productohome";
 	}
 	
 }
