31d7dcd06973f7853e1ce97b76b90afa9e292322
Zaira Popoca <zairapl12@gmail.com>
2022-02-16 09:22:45 -0600

    metodo para generar el numero de la orden

--- src/main/java/com/zmpa/eggstore/service/IOrdenService.java
+++ src/main/java/com/zmpa/eggstore/service/IOrdenService.java
@@ -1,7 +1,10 @@
 package com.zmpa.eggstore.service;
 
+import java.util.List;
+
 import com.zmpa.eggstore.model.Orden;
 
 public interface IOrdenService {
+	List<Orden> findAll();
 	Orden save (Orden orden);
 }
--- src/main/java/com/zmpa/eggstore/service/OrdenServiceImpl.java
+++ src/main/java/com/zmpa/eggstore/service/OrdenServiceImpl.java
@@ -1,5 +1,8 @@
 package com.zmpa.eggstore.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +18,40 @@ public class OrdenServiceImpl implements IOrdenService{
 	@Override
 	public Orden save(Orden orden) {
 		return ordenDao.save(orden);
+	}
+
+	@Override
+	public List<Orden> findAll() {
+		return ordenDao.findAll();
+	}
+	
+	public String generarNumeroOrden() {
+		int numero=0;
+		String numeroConcatenado="";
+		
+		List<Orden> ordenes = findAll();
+		
+		List<Integer> numeros = new ArrayList<>();
+		
+		ordenes.stream().forEach(o -> numeros.add(Integer.parseInt(o.getNumero())));
+		
+		if (ordenes.isEmpty()) {
+			numero=1;
+			
+		}else {
+			numero= numeros.stream().max(Integer::compare).get();
+			numero++;
+		}
+		
+		if (numero<10) {
+			numeroConcatenado="000000000"+String.valueOf(numero);
+		}else if (numero<100) {
+			numeroConcatenado="00000000"+String.valueOf(numero);
+		}else if (numero<1000) {
+			numeroConcatenado="0000000"+String.valueOf(numero);
+		}
+		
+		return numeroConcatenado;
 	}
 
 }
