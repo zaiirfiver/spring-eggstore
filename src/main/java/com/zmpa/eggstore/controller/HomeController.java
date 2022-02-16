0d06a179b6500d2a56adab762d25beb08ca4cbc0
Zaira Popoca <zairapl12@gmail.com>
2022-02-16 04:24:27 -0600

    funcionalidad ver productos para el usuario(metodo home, directorio y tamplate index.html)

---
+++ src/main/java/com/zmpa/eggstore/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.zmpa.eggstore.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zmpa.eggstore.service.ProductoService;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@Autowired
+	private ProductoService productoService;				//permite obtener todos los productos
+	
+	@GetMapping("")
+	public String index(Model model) {
+		
+		model.addAttribute("productos", productoService.findAll());
+		
+		return "usuario/index";
+	}
+	
+}
