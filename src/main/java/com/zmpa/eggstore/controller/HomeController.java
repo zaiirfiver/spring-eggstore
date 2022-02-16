63e38c3d7dc7621268c9a538dad504e952ddd76b
Zaira Popoca <zairapl12@gmail.com>
2022-02-16 05:51:04 -0600

    cantidad y buscar el producto en el home controller para el carrito

--- src/main/java/com/zmpa/eggstore/controller/HomeController.java
+++ src/main/java/com/zmpa/eggstore/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.zmpa.eggstore.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -11,7 +13,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.zmpa.eggstore.model.DetalleOrden;
+import com.zmpa.eggstore.model.Orden;
 import com.zmpa.eggstore.model.Producto;
 import com.zmpa.eggstore.service.ProductoService;
 
@@ -21,9 +26,12 @@ public class HomeController {
 	
 	private final Logger log = LoggerFactory.getLogger(HomeController.class);			//imprime los resultados en la terminal
 	
-	
 	@Autowired
 	private ProductoService productoService;				//permite obtener todos los productos
+	
+	List<DetalleOrden> detalles= new ArrayList<DetalleOrden>();			//Almacena los detalles de la orden
+	
+	Orden orden = new Orden();										//datos de la orden
 	
 	@GetMapping("")
 	public String index(Model model) {
@@ -46,7 +54,15 @@ public class HomeController {
 	}
 	
 	@PostMapping("/cart")
-	public String addCart() {
+	public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad) {
+		DetalleOrden detalleOrden = new DetalleOrden();
+		Producto producto = new Producto();
+		double sumaTotal = 0;
+		
+		Optional<Producto> optionalProducto = productoService.get(id);
+		log.info("Producto añadido: {}", optionalProducto.get());
+		log.info("Cantidad: {}", cantidad);
+		
 		return "usuario/carrito";
 	}
 }
