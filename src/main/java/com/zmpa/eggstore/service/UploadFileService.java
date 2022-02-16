889a9dedbc7cd120b02c04b88f1295dde38ba355
Zaira Popoca <zairapl12@gmail.com>
2022-02-16 02:40:28 -0600

    clase de servicio para subir una imagen al servidor

---
+++ src/main/java/com/zmpa/eggstore/service/UploadFileService.java
@@ -0,0 +1,31 @@
+package com.zmpa.eggstore.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class UploadFileService {
+	private String folder="images//";
+	
+	public String saveImage(MultipartFile file) throws IOException {			//parametro que guarda la imagen
+		if(!file.isEmpty()) {
+			byte [] bytes=file.getBytes();
+			Path path = Paths.get(folder+file.getOriginalFilename());
+			Files.write(path, bytes);											//Obtiene la imagen en bytes para poder enviarla al servidor
+			return file.getOriginalFilename();
+		}
+		return "default.jpg";
+	}
+	
+	public void deleteImage(String nombre) {											//elimina la imagen cuando se elimina un producto
+		String ruta="images//";
+		File file= new File(ruta+nombre);
+		file.delete();	}
+	
+}
