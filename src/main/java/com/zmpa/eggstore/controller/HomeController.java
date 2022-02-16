263d53e132788211937d865d6b78ba3dd4decefc
Zaira Popoca <zairapl12@gmail.com>
2022-02-16 06:32:45 -0600

    funcionalidad del boton quitar

--- src/main/java/com/zmpa/eggstore/controller/HomeController.java
+++ src/main/java/com/zmpa/eggstore/controller/HomeController.java
@@ -81,4 +81,31 @@ public class HomeController {
 		
 		return "usuario/carrito";
 	}
+	
+	//quitar un producto del carrito
+	@GetMapping("/delete/cart/{id}")
+	public String deleteProductoCart(@PathVariable Integer id, Model model) {
+		
+		//lista nueva de productos
+		List<DetalleOrden> ordenesNuevas = new ArrayList<DetalleOrden>();
+		
+		for(DetalleOrden detalleOrden: detalles) {
+			if (detalleOrden.getProducto().getId()!=id) {
+				ordenesNuevas.add(detalleOrden);
+			}
+		}
+		
+		//nueva lista con los productos que restan
+		detalles=ordenesNuevas;
+		
+		double sumaTotal=0;
+		sumaTotal = detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
+		
+		orden.setTotal(sumaTotal);
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		
+		
+		return "usuario/carrito";
+	}
 }
