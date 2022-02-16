5c2e37f45ed2f3067a52b49722a90e9cdecb2c21
Zaira Popoca <zairapl12@gmail.com>
2022-02-16 05:19:59 -0600

    mostrar informacion del producto en la pagina de productohome

--- src/main/java/com/zmpa/eggstore/controller/HomeController.java
+++ src/main/java/com/zmpa/eggstore/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.zmpa.eggstore.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.zmpa.eggstore.model.Producto;
 import com.zmpa.eggstore.service.ProductoService;
 
 @Controller
@@ -30,8 +33,14 @@ public class HomeController {
 	}
 	
 	@GetMapping("productohome/{id}")
-	public String productoHome(@PathVariable Integer id) {
+	public String productoHome(@PathVariable Integer id, Model model) {
 		log.info("id enviado como parametro {}", id);
+		Producto producto = new Producto();
+		Optional<Producto> productoOptional = productoService.get(id);
+		producto = productoOptional.get();
+		
+		model.addAttribute("producto", producto);
+		
 		return "usuario/productohome";
 	}
 	
